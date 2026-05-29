4d8923935bb9079c2c9abbeb2f16251a93acf454
GuilhermeLegramante <guilhermelegramante@gmail.com>
2026-05-29 13:47:30 -0300

    .

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -36,15 +36,13 @@ class LoginController extends Controller
         $clientName = config('app.client_name'); // 'cacequicm'
         $senhaSha1 = sha1($request->input('senha'));
 
-        $teste = DB::select("SELECT * FROM `glbcliente` where identificador = 'cacequicm' and senha = 'a18df841600ba0f2af4b6f2a69e6ae6db49d3d57';
-");
-        dd($teste);
-
         // Usando LIKE binário ou comparando limpando espaços (TRIM) para forçar o banco a ignorar qualquer metadado ou espaço invisível
         $cliente = DB::table('glbcliente')
             ->whereRaw('TRIM(identificador) LIKE ?', [trim($clientName)])
             ->whereRaw('TRIM(senha) LIKE ?', [trim($senhaSha1)])
             ->first();
+
+        dd($cliente);
 
         if ($cliente) {
             // Valida se o cliente está ativo (no seu print, Cacequi está ativo = 1)
